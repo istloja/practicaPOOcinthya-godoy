3873a35cc620ac4684a92f74765164c3d3947499
ISTLOJAV <ISTLOJAV@ISTLOJA11>
2019-06-21 09:52:08 -0500

    herencia 21 de junio

--- practicaPOOcinthyagodoy/src/herencia21junio/proveedor.java
+++ practicaPOOcinthyagodoy/src/herencia21junio/proveedor.java
@@ -9,6 +9,41 @@ package herencia21junio;
  *
  * @author ISTLOJAV
  */
-public class proveedor {
+public class proveedor extends Persona {
+     private String direccion ;
+     private String correoelectronico;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoelectronico() {
+        return correoelectronico;
+    }
+
+    public void setCorreoelectronico(String correoelectronico) {
+        this.correoelectronico = correoelectronico;
+    }
+    
+     @Override
+     public void saludar(){
+        System.out.println("la pregunta sera respondida en unos minutos");
+    }
+     public void test(){
+         saludar();
+         super.saludar();//llama al metodo de la clase padre
+     }
+     public static void main(String[] args) {
+        proveedor objeto=new proveedor();
+        objeto.saludar();
+        
+        objeto.test();
+    }
+    
+
     
 }
